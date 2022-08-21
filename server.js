3ab5836efdfd8da414204853b6e8f16ca7cae9c2
Andrey Babkin <babkin.andrey@gmail.com>
2022-08-21 13:02:45 +0300

    auth

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const jsonServer = require('json-server');
+const auth = require('json-server-auth');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json'); // <== Will be created later
+
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3200; // <== You can change the port
 
+server.db = router.db
+
+server.use(auth);
 server.use(middlewares);
 server.use(router);
 
