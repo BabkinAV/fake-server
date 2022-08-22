85252715d4418298c5bc33ec9b3a01d6626bacf1
Andrey Babkin <babkin.andrey@gmail.com>
2022-08-22 20:17:04 +0300

    cors

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const jsonServer = require('json-server');
+
+const cors = require('cors');
 const auth = require('json-server-auth');
 const server = jsonServer.create();
+server.use(cors());
 const router = jsonServer.router('db.json'); 
 
 const middlewares = jsonServer.defaults();
@@ -8,12 +11,6 @@ const port = process.env.PORT || 3200; // <== You can change the port
 
 server.db = router.db
 
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://dainty-paprenjak-d6a3df.netlify.app');
-
-  res.header('Access-Control-Allow-Headers', '*')
-  next()
-})
 server.use(auth);
 server.use(middlewares);
 server.use(router);
